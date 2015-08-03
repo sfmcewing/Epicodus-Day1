8009e3cd247bebe4562c316359ddd3dcb745078d
Filipe Silvestre <student@epicodus.com>
2015-08-03 12:16:13 -0700

    We added awesome images

--- Cars.php
+++ Cars.php
@@ -4,11 +4,13 @@ class Car {
     private $make_model;
     private $price;
     private $miles;
+    private $image_path;
 
-    function __construct($make_model, $price, $miles = 5000) {
+    function __construct($make_model, $price, $image_path, $miles = 5000) {
     	$this->make_model = $make_model;
     	$this->price = $price;
     	$this->miles = $miles;
+    	$this->image_path = $image_path;
     }
 
     // setters
@@ -27,6 +29,11 @@ class Car {
     	$this->miles = $setMiles;
     }
 
+    function setImage ($imagePath) {
+
+    	$this->image_path = $imagePath;
+    }
+
     // getters
     function getModel() {
 
@@ -40,12 +47,16 @@ class Car {
 
     	return $this->miles;
     }
+    function getImage () {
+
+    	return $this->image_path;
+    }
 }
 
-$batmobile = new Car("Mercedes", 200000);
-$pinto = new Car("Ford", 500, 6000);
-$flugtag = new Car("Redbull", 50, 3);
-$daewoo = new Car("Lanos", 123456);
+$batmobile = new Car("Mercedes", 200000, "images/batmobile.jpeg");
+$pinto = new Car("Ford", 500, "images/pinto.jpeg", 600);
+$flugtag = new Car("Redbull", 50, "images/flugtag.jpeg", 6);
+$daewoo = new Car("Lanos", 123456, "images/daewoo lanos.jpeg");
 
 $cars = array($batmobile, $pinto, $flugtag, $daewoo);
 
@@ -64,11 +75,13 @@ $cars = array($batmobile, $pinto, $flugtag, $daewoo);
            	$carModel = $car->getModel();
            	$carPrice = $car->getPrice();
            	$carMiles = $car->getMiles();
+           	$carImage = $car->getImage();
 
            	echo "<li> $carModel </li>";
            	echo "<ul>";
            	    echo "<li> $$carPrice </li>";
            	    echo "<li> $carMiles </li>";
+           	    echo "<li> <img src='$carImage'></li>";
            	echo "</ul>";
            }
         ?>
