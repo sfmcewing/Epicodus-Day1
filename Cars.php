c7c6c1c90cdb0ca2f60405620cd33b689c68325d
Filipe Silvestre <student@epicodus.com>
2015-08-03 11:59:08 -0700

    Encapsulated stuff

--- Cars.php
+++ Cars.php
@@ -1,23 +1,54 @@
 <?php
 class Car {
 
-    public $make_model;
-    public $price;
-    public $miles;
+    private $make_model;
+    private $price;
+    private $miles;
 
-    function __construct($make_model, $price, $miles) {
+    function __construct($make_model, $price, $miles = 5000) {
     	$this->make_model = $make_model;
     	$this->price = $price;
     	$this->miles = $miles;
     }
+
+    // setters
+    function setModel ($modelName) {
+
+    	$this->make_model = $modelName;
+    }
+
+    function setPrice ($setPrice) {
+
+    	$this->price = $setPrice;
+    }
+
+    function setMiles ($setMiles) {
+
+    	$this->miles = $setMiles;
+    }
+
+    // getters
+    function getModel() {
+
+    	return $this->make_model;
+    }
+    function getPrice() {
+
+    	return $this->price;
+    }
+    function getMiles() {
+
+    	return $this->miles;
+    }
 }
 
-$batmobile = new Car("Mercedes", 200000, 25000);
+$batmobile = new Car("Mercedes", 200000);
 $pinto = new Car("Ford", 500, 6000);
 $flugtag = new Car("Redbull", 50, 3);
-$daewoo = new Car("Lanos", 123456, 83373);
+$daewoo = new Car("Lanos", 123456);
 
 $cars = array($batmobile, $pinto, $flugtag, $daewoo);
+
 ?>
 
 <!DOCTYPE html>
@@ -30,10 +61,14 @@ $cars = array($batmobile, $pinto, $flugtag, $daewoo);
    <ul>
         <?php
            foreach ($cars as $car) {
-           	echo "<li> $car->make_model </li>";
+           	$carModel = $car->getModel();
+           	$carPrice = $car->getPrice();
+           	$carMiles = $car->getMiles();
+
+           	echo "<li> $carModel </li>";
            	echo "<ul>";
-           	    echo "<li> $$car->price </li>";
-           	    echo "<li> Miles: $car->miles </li>";
+           	    echo "<li> $$carPrice </li>";
+           	    echo "<li> $carMiles </li>";
            	echo "</ul>";
            }
         ?>
